6276b87151c053384bb3f599d5dfaf98a7b6e141
Dimas <ds3211407@gmail.com>
2024-03-01 16:00:46 +0300

    commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,13 +6,14 @@ import Application.ConsoleApp;
  * Здесь запускается программа. В качестве переменной окружения нужно поставить имя файла, который дальше будет использоваться в программе
  */
 public class Main {
+    public static final String value = "path";
 
     public static void main(String[] args) {
         String[] values = new String[args.length];
         System.arraycopy(args, 0, values, 0, values.length);
 
         String file_name = FileNameFind.find(values);
-        FileName.set(file_name);
+        System.setProperty(value, file_name); // устанавливаем переменную окружения.
 
         if (FileValidator.check(file_name)) {
             ConsoleApp app = new ConsoleApp(file_name);
